09bf60b95c40d12b254928928636d9c4c84fd992
Ciahaka <wilddiver.by@gmail.com>
2023-02-28 15:32:39 +0300

    change Boolean.js

--- src/FreeCodeCamp/JavaScript Algorithms and Data Structures/Boolean.js
+++ src/FreeCodeCamp/JavaScript Algorithms and Data Structures/Boolean.js
@@ -11,39 +11,3 @@ console.log(trueOrFalse(true))
 console.log(trueOrFalse(false))
 
 //---------------------------------------------
-const testStrict = (val) => {
-    if (val === 7) { // Change this line
-        return 'Equal';
-    }
-    return 'Not Equal';
-}
-console.log(testStrict(10))
-console.log(testStrict(7))
-
-//--------------------------------
-console.log(typeof 3)
-console.log(typeof '3')
-
-function compareEquality(a, b) {
-    if (a === b) { // Change this line
-        return 'Equal';
-    }
-    return 'Not Equal';
-}
-
-console.log(compareEquality(10, '10'))
-console.log(compareEquality(10, 5))
-console.log(compareEquality(10, 10))
-
-//---------------------------------------------------
-
-const testNotEqual = (val) => {
-    if (!val) { // Change this line
-        return 'Not Equal';
-    }
-    return 'Equal';
-}
-console.log(testNotEqual(10))
-;
-console.log(10 !==10)
-console.log(10 !=='10')
