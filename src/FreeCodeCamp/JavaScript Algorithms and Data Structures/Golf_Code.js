4184d264f87e0c39dc7cc1f851f6d2888b72d8b3
Ciahaka <wilddiver.by@gmail.com>
2023-03-02 23:04:42 +0300

    add lesson 'Golf Code'

--- src/FreeCodeCamp/JavaScript Algorithms and Data Structures/Golf_Code.js
+++ src/FreeCodeCamp/JavaScript Algorithms and Data Structures/Golf_Code.js
@@ -28,3 +28,22 @@ const golfScore = (par, strokes) => {
 }
 console.log(golfScore(5, 9))
 ;
+
+//или вариант с радутым тернарником
+/**
+ * function golfScore(par, strokes) {
+ *   return strokes === 1
+ *     ? names[0]
+ *     : strokes <= par - 2
+ *     ? names[1]
+ *     : strokes === par - 1
+ *     ? names[2]
+ *     : strokes === par
+ *     ? names[3]
+ *     : strokes === par + 1
+ *     ? names[4]
+ *     : strokes === par + 2
+ *     ? names[5]
+ *     : names[6];
+ * }
+ */
