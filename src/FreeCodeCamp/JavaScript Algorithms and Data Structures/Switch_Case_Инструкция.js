29d1bbe56fd6ad4733484287eb70f03effbad2ed
Ciahaka <wilddiver.by@gmail.com>
2023-03-03 14:54:45 +0300

    add lesson 'Selecting from Many Options with Switch Statements'

---
+++ src/FreeCodeCamp/JavaScript Algorithms and Data Structures/Switch_Case_Инструкция.js
@@ -0,0 +1,21 @@
+const caseInSwitch = (val) => {
+    switch (val) {
+        case 1:
+            return 'alpha'
+            break;
+        case 2:
+            return 'beta'
+            break;
+        case 3:
+            return 'gamma'
+            break;
+        case 4:
+            return 'delta'
+            break;
+    }
+    return val
+}
+console.log(caseInSwitch(5))
+;
+//Даннфя конструкция позволяет сделать выбор и многои вариантов
+//если не прервать case то начинает выполнятся следующий case. Независимо от того, подошёл ли первый.
