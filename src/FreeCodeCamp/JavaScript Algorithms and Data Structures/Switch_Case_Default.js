f775d0db8917eb45445473348d757aef1d089d35
Ciahaka <wilddiver.by@gmail.com>
2023-03-03 15:14:51 +0300

    add lesson 'Adding a Default Option in Switch Statements'

---
+++ src/FreeCodeCamp/JavaScript Algorithms and Data Structures/Switch_Case_Default.js
@@ -0,0 +1,19 @@
+function switchOfStuff(val) {
+    switch (val) {
+        case 'a':
+            return 'apple';
+            break
+        case 'b':
+            return 'bird';
+            break
+        case 'c':
+            return 'cat';
+            break
+        default:
+            return 'stuff';
+            break
+    }
+}
+
+console.log(switchOfStuff('l'))
+;
