e789a0354cfa70b690da380e74d7cef75d2b41da
Ciahaka <wilddiver.by@gmail.com>
2023-03-14 14:44:53 +0300

    add lesson 'Объект'

---
+++ src/FreeCodeCamp/JavaScript Algorithms and Data Structures/Build_JavaScript__Objects.js
@@ -0,0 +1,17 @@
+const myDog = {
+    name:'Gena',
+    legs:4,
+    tails:1,
+    friends:['man','Cheburashka']
+}
+console.log(myDog)
+
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+
+const hatValue = testObj.hat;
+const shirtValue = testObj.shirt;
