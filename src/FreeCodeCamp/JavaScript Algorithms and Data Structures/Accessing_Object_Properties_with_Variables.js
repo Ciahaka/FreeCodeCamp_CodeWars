f0625e5af3155b65f9318594ca1ee3b6d2f5ae6a
Ciahaka <wilddiver.by@gmail.com>
2023-03-15 14:27:49 +0300

    add lesson 'Доступ к значению св-ва объекта через его имя'

---
+++ src/FreeCodeCamp/JavaScript Algorithms and Data Structures/Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,29 @@
+const dogs = {
+    Fido: "Mutt",
+    Hunter: "Doberman",
+    Snoopie: "Beagle"
+};
+const myDog = "Hunter";
+const myBreed = dogs[myDog];
+console.log(myBreed);
+
+const kkk ={
+    dog:'1223',
+    cat: 'fffff'
+}
+const myCat= 'dog'
+const myMy=kkk[myCat]
+console.log(myMy)
+
+
+// Установите для переменной playerNumber значение 16. Затем используйте эту переменную, чтобы найти имя игрока и присвоить его игроку.
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+console.log(player)
