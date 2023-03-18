5f4106385d5f16c6f9274fb6834d23e862b7d470
Ciahaka <wilddiver.by@gmail.com>
2023-03-18 12:18:30 +0300

    add task 'Добавление'

---
+++ src/FreeCodeCamp/JavaScript Algorithms and Data Structures/Manipulating_Complex_Objects.js
@@ -0,0 +1,27 @@
+//Добавьте новый альбом в массив myMusic. Добавьте строки исполнителя и названия, номер выпуска_года и массив строк форматов.
+
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    }
+];
+console.log(myMusic)
