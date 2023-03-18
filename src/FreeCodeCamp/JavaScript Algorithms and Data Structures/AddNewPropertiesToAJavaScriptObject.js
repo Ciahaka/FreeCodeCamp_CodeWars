b5b28c668529ac4978edf67f4f6e64543aa3fb72
Ciahaka <wilddiver.by@gmail.com>
2023-03-18 10:28:14 +0300

    add task 'Добавление новых свойств в объект'

---
+++ src/FreeCodeCamp/JavaScript Algorithms and Data Structures/AddNewPropertiesToAJavaScriptObject.js
@@ -0,0 +1,11 @@
+//Добавьте свойство bark в myDog и задайте для него звук собаки, например «гав». Вы можете использовать либо точку, либо квадратную нотацию.
+
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+myDog.bark = 'Aw-Aw'
+myDog['enemy'] = 'cat'
+console.log(myDog)
