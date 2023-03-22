6fdefe5ff1007aa185ea3b5b525dbfc31f99c95f
Ciahaka <wilddiver.by@gmail.com>
2023-03-22 13:33:47 +0300

    add task 'Массив булевых значений. Достань количество true'

---
+++ src/CodeWars/JS_8kyu/Counting_sheep.js
@@ -0,0 +1,18 @@
+//Рассмотрим массив/список овец, где некоторые овцы могут отсутствовать на своем месте. Нам нужна функция, которая подсчитывает количество овец, присутствующих в массиве (true означает наличие).
+
+const arrayOfSheep = [true, true, true, false,
+    true, true, true, true,
+    true, false, true, false,
+    true, false, false, true,
+    true, true, true, true,
+    false, false, true, true]
+
+// const countSheep = arr => arr.reduce((acc, cur) => acc + cur, 0)   <---???
+
+const countSheep = array1 => array1.filter(Boolean).length
+// or
+// const countSheep = arr => arr.filter(sh=>sh===true).length
+
+
+console.log(countSheep(arrayOfSheep))
+
