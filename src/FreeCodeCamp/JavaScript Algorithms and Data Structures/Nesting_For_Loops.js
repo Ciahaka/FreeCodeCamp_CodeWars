addc15c05eef0f4148499d3406062fecc4dd7a3e
Ciahaka <wilddiver.by@gmail.com>
2023-03-27 12:58:51 +0300

    refactor task 'Перебор массива с помощью цикла for'

--- src/FreeCodeCamp/JavaScript Algorithms and Data Structures/Nesting_For_Loops.js
+++ src/FreeCodeCamp/JavaScript Algorithms and Data Structures/Nesting_For_Loops.js
@@ -21,7 +21,8 @@ const multiplyAll = (arr) => {
     let product = 1;
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr[i].length; j++) {
-           product = product * arr[i][j]
+        //   product = product * arr[i][j]
+            product *= arr[i][j]
         }}
     return product;
  }
